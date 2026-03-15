a62da4718a2be9ee0c5e603e7fe53747085ac78e
Salamanderooo <salalomon@gmail.com>
2026-03-15 20:35:48 +0100

    cleaning around controls added logic with playlist, index

--- gui/controls.py
+++ gui/controls.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QPushButton, QFileDialog
 from sequencer.sample_player import SamplePlayer
 
+
 class PlayerControls:
     def __init__(self, parent):
         self.parent = parent
@@ -16,6 +17,7 @@ class PlayerControls:
         # podłączenie przycisków do metod
         self.play_button.clicked.connect(self.play_clicked)
         self.stop_button.clicked.connect(self.stop_clicked)
+        self.select_song.clicked.connect(self.open_file_dialog)
 
     def play_clicked(self):
         # tutaj GUI może później podać wybrany plik
@@ -25,8 +27,7 @@ class PlayerControls:
     def stop_clicked(self):
         self.player.stop()
 
-    def open_file_dialog(self, parent):
-        self.select_song.clicked.connect(self.open_file_dialog)
-        filename, _ = QFileDialog.getOpenFileName(parent, "Wybierz plik dźwiękowy", "", "Audio Files (*.wav *.mp3)")
+    def open_file_dialog(self):
+        filename, _ = QFileDialog.getOpenFileName(self.parent, "Wybierz plik dźwiękowy", "", "Audio Files (*.wav *.mp3)")
         if filename:
             self.player.play(filename)
